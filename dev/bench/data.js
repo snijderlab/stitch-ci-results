026b8fde9991436c3ef40c69d744df3cd5207d67
github-action-benchmark <github@users.noreply.github.com>
2022-09-08 11:13:12 +0000

    add Results Benchmark (customBiggerIsBetter) benchmark result for a5ed7720dce53087434e3770fa2bda2c9f8a9092

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1662632270058,
+  "lastUpdate": 1662635592321,
   "repoUrl": "https://github.com/snijderlab/stitch",
   "entries": {
     "Results Benchmark": [
@@ -23,6 +23,52 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/snijderlab/stitch/commit/fcc8451d70f60019e5d54857b99ad76262335b13"
         },
         "date": 1662632253505,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "REC-0-1 - Score",
+            "value": 2396,
+            "unit": "Score",
+            "extra": "450M"
+          },
+          {
+            "name": "REC-0-1 - Identity",
+            "value": 0,
+            "unit": "Percent"
+          },
+          {
+            "name": "REC-0-1_002 - Score",
+            "value": 1103,
+            "unit": "Score",
+            "extra": "214M"
+          },
+          {
+            "name": "REC-0-1_002 - Identity",
+            "value": 0,
+            "unit": "Percent"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "d.schulte@uu.nl",
+            "name": "Douwe Schulte",
+            "username": "douweschulte"
+          },
+          "committer": {
+            "email": "d.schulte@uu.nl",
+            "name": "Douwe Schulte",
+            "username": "douweschulte"
+          },
+          "distinct": true,
+          "id": "a5ed7720dce53087434e3770fa2bda2c9f8a9092",
+          "message": "Fixed Identity calculation",
+          "timestamp": "2022-09-08T13:10:55+02:00",
+          "tree_id": "272bcc207dde87822d98062b8369f635411c6f9d",
+          "url": "https://github.com/snijderlab/stitch/commit/a5ed7720dce53087434e3770fa2bda2c9f8a9092"
+        },
+        "date": 1662635575993,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
