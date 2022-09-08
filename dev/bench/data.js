e599e31b3218baa82c4290aa35e589751c0c423b
github-action-benchmark <github@users.noreply.github.com>
2022-09-08 10:17:50 +0000

    add Results Benchmark (customBiggerIsBetter) benchmark result for fcc8451d70f60019e5d54857b99ad76262335b13

---
+++ dev/bench/data.js
@@ -0,0 +1,54 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1662632270058,
+  "repoUrl": "https://github.com/snijderlab/stitch",
+  "entries": {
+    "Results Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "d.schulte@uu.nl",
+            "name": "Douwe Schulte",
+            "username": "douweschulte"
+          },
+          "committer": {
+            "email": "d.schulte@uu.nl",
+            "name": "Douwe Schulte",
+            "username": "douweschulte"
+          },
+          "distinct": true,
+          "id": "fcc8451d70f60019e5d54857b99ad76262335b13",
+          "message": "Fixed repo url",
+          "timestamp": "2022-09-08T12:08:40+02:00",
+          "tree_id": "b025592fd5a9bb34125705ea9c5a998790175575",
+          "url": "https://github.com/snijderlab/stitch/commit/fcc8451d70f60019e5d54857b99ad76262335b13"
+        },
+        "date": 1662632253505,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "REC-0-1 - Score",
+            "value": 2396,
+            "unit": "Score",
+            "extra": "450M"
+          },
+          {
+            "name": "REC-0-1 - Identity",
+            "value": 0,
+            "unit": "Percent"
+          },
+          {
+            "name": "REC-0-1_002 - Score",
+            "value": 1103,
+            "unit": "Score",
+            "extra": "214M"
+          },
+          {
+            "name": "REC-0-1_002 - Identity",
+            "value": 0,
+            "unit": "Percent"
+          }
+        ]
+      }
+    ]
+  }
+}
